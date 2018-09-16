a134533ef1c177210a1bfa3cb2ada9d9aea590af
edv1297 <edv2@williams.edu>
2018-09-16 09:20:13 -0400

    rails test application

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,13 @@
+class ApplicationController < ActionController::Base
+
+
+    def hello
+        render html: "¡Hola, mundo!"
+    end
+
+    
+    def goodbye
+        render html: "Goodbye, mundo!"
+    end
+
+end

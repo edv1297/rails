74b1a3de0b00e66f68174ba025997cd30d5e1e75
edv1297 <edv2@williams.edu>
2018-10-16 21:46:38 -0400

    working on route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'demo/index'
+  resources :microposts
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
-  get "goodbye", to: 'application#goodbye'
-  
+  root 'users#index'
 end
